b3e9f497d250ca787f02b6a10bcf1d7e79d8ec66
Suresh V <vjsuru94@gmail.com>
2025-04-21 23:07:26 +0530

    dockerfile and deployment scripts

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@
 
 elif [ "$BRANCH_NAME" == "main" ]; then
   docker build -t suresh/prod-react-image .
-  echo "Suresh@1234" | docker login -u "suresh" --password-stdin
+  echo "Suresh@1994" | docker login -u "suresh" --password-stdin
   docker push suresh/prod-react-image:latest
 fi
 chmod +x build.sh
