23314d956a364266c7f67e255f9978fe0518e5a0
Suresh V <vjsuru94@gmail.com>
2025-04-25 16:47:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-
-echo "Deployment Branch: $BRANCH_NAME"
-
-docker stop my-reactapp-container || true
-docker rm my-reactapp-container || true
-
-if [ "$BRANCH_NAME" == "dev" ]; then
-  docker pull suresh/dev-react-image:latest
-  docker run -d -p 80:80 --name my-reactapp-container suresh/dev-react-image:latest
-
-elif [ "$BRANCH_NAME" == "main" ]; then
-  docker pull suresh/prod-react-image:latest
-  docker run -d -p 80:80 --name my-reactapp-container sureshv/prod-react-image:latest
-fi
-chmod +x deploy.sh
-echo "Deployment script executed successfully."
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 596bf86c2564bc5635e5e7864c52b2036eb7905b
+docker-compose up -d
