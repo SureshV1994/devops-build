821ea780bdb53d95daddc307d16d9584afab0675
Suresh V <vjsuru94@gmail.com>
2025-04-25 17:54:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
  #!/bin/bash
  
  echo “Current Branch: $GIT_BRANCH”
-  
-  if [ "$GIT_BRANCH" == "dev"  ]; then
+  if [[ $GIT_BRANCH == "origin/dev" ]]; then
   docker build -t suresh/dev-react-image .
   echo "Suresh@1994" | docker login -u "suresh" -p dckr_pat_iXHZs9e6ZPqqulHue-kNYiNMMho
   docker push suresh/dev-react-image:latest
 
-elif [ "$GIT_BRANCH" == "main" ]; then
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
   docker build -t suresh/prod-react-image .
   echo "Suresh@1994" | docker login -u "suresh" -p dckr_pat_iXHZs9e6ZPqqulHue-kNYiNMMho
   docker push suresh/prod-react-image:latest
