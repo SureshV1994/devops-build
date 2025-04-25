0fdd03eb2ca7268fe5372a0b010bd67259c1ca26
Suresh V <vjsuru94@gmail.com>
2025-04-25 18:32:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
   docker build -t suresh/dev-react-image .
-  echo "Suresh@1994" | docker login -u "suresh" -p dckr_pat_iXHZs9e6ZPqqulHue-kNYiNMMho
+  echo "Suresh@1994" | docker login -u "sureshv1994" -p dckr_pat_iXHZs9e6ZPqqulHue-kNYiNMMho
   docker push suresh/dev-react-image:latest
